d076ff6a3882ea9932180879eb164bd87b1b0ee1
Brian Haggard <brihagg@gmail.com>
2016-07-31 13:07:50 -0500

    Accept factors as arguements to struct creation - BH

--- problems/problem_one.go
+++ problems/problem_one.go
@@ -2,18 +2,19 @@ package problems
 
 type ProblemOne struct {
 	ceiling int
+	factors []int
 }
 
 func (p ProblemOne) Solve() int {
 	var answer int
 	for i := 0; i < p.ceiling; i++ {
-		if i%3 == 0 || i%5 == 0 {
+		if i%p.factors[0] == 0 || i%p.factors[1] == 0 {
 			answer += i
 		}
 	}
 	return answer
 }
 
-func NewProblemOne(ceiling int) Solver {
-	return &ProblemOne{ceiling: ceiling}
+func NewProblemOne(ceiling int, factors ...int) Solver {
+	return &ProblemOne{ceiling: ceiling, factors: factors}
 }
--- problems/problem_one_test.go
+++ problems/problem_one_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func Test_ProblemOneReturnsExampleAnswer(t *testing.T) {
-	testObject := problems.NewProblemOne(10)
+	testObject := problems.NewProblemOne(10, 3, 5)
 
 	assert.Equal(t, 23, testObject.Solve())
 
@@ -16,14 +16,14 @@ func Test_ProblemOneReturnsExampleAnswer(t *testing.T) {
 
 func Test_ProblemOneReturnsSumOfMultiplesOf3And5Under20(t *testing.T) {
 	// sum(3, 5, 6, 9, 10, 12, 15, 18) = 78
-	testObject := problems.NewProblemOne(20)
+	testObject := problems.NewProblemOne(20, 3, 5)
 
 	assert.Equal(t, 78, testObject.Solve())
 
 }
 
 func Test_ProblemOneReturnsSumOfMultiplesOf3And5Under1000(t *testing.T) {
-	testObject := problems.NewProblemOne(1000)
+	testObject := problems.NewProblemOne(1000, 3, 5)
 
 	assert.Equal(t, 233168, testObject.Solve())
 
