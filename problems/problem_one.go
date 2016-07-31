2050f112d9c3e555cfd72e7ab0faab362e623071
Brian Haggard <brihagg@gmail.com>
2016-07-31 12:55:55 -0500

    Implementation of actual solution - BH

--- problems/problem_one.go
+++ problems/problem_one.go
@@ -1,12 +1,19 @@
 package problems
 
 type ProblemOne struct {
+	ceiling int
 }
 
 func (p ProblemOne) Solve() int {
-	return 23
+	var answer int
+	for i := 0; i < p.ceiling; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			answer += i
+		}
+	}
+	return answer
 }
 
 func NewProblemOne(ceiling int) Solver {
-	return &ProblemOne{}
+	return &ProblemOne{ceiling: ceiling}
 }
--- problems/problem_one_test.go
+++ problems/problem_one_test.go
@@ -8,9 +8,23 @@ import (
 )
 
 func Test_ProblemOneReturnsExampleAnswer(t *testing.T) {
-
 	testObject := problems.NewProblemOne(10)
 
 	assert.Equal(t, 23, testObject.Solve())
+
+}
+
+func Test_ProblemOneReturnsSumOfMultiplesOf3And5Under20(t *testing.T) {
+	// sum(3, 5, 6, 9, 10, 12, 15, 18) = 78
+	testObject := problems.NewProblemOne(20)
+
+	assert.Equal(t, 78, testObject.Solve())
+
+}
+
+func Test_ProblemOneReturnsSumOfMultiplesOf3And5Under1000(t *testing.T) {
+	testObject := problems.NewProblemOne(1000)
+
+	assert.Equal(t, 233168, testObject.Solve())
 
 }
