b62da0b51f44f1c284188e68ad0f198a4856ccc5
Brian Haggard <brihagg@gmail.com>
2016-07-31 13:27:25 -0500

    De-magic numbers in tests - BH

--- problems/problem_one_test.go
+++ problems/problem_one_test.go
@@ -8,7 +8,9 @@ import (
 )
 
 func Test_ProblemOneReturnsExampleAnswer(t *testing.T) {
-	testObject := problems.NewProblemOne(10, 3, 5)
+	ceiling := 10
+	factors := []int{3, 5}
+	testObject := problems.NewProblemOne(ceiling, factors[0], factors[1])
 
 	assert.Equal(t, 23, testObject.Solve())
 
@@ -16,21 +18,27 @@ func Test_ProblemOneReturnsExampleAnswer(t *testing.T) {
 
 func Test_ProblemOneReturnsSumOfMultiplesOf3And5Under20(t *testing.T) {
 	// sum(3, 5, 6, 9, 10, 12, 15, 18) = 78
-	testObject := problems.NewProblemOne(20, 3, 5)
+	ceiling := 20
+	factors := []int{3, 5}
+	testObject := problems.NewProblemOne(ceiling, factors[0], factors[1])
 
 	assert.Equal(t, 78, testObject.Solve())
 
 }
 
 func Test_ProblemOneReturnsSumOfMultiplesOf3And5Under1000(t *testing.T) {
-	testObject := problems.NewProblemOne(1000, 3, 5)
+	ceiling := 1000
+	factors := []int{3, 5}
+	testObject := problems.NewProblemOne(ceiling, factors[0], factors[1])
 
 	assert.Equal(t, 233168, testObject.Solve())
 
 }
 
 func Test_ProblemOneReturnsSumOfMultiplesOfNFactors(t *testing.T) {
-	testObject := problems.NewProblemOne(10, 3, 5, 7)
+	ceiling := 10
+	factors := []int{3, 5, 7}
+	testObject := problems.NewProblemOne(ceiling, factors[0], factors[1], factors[2])
 
 	assert.Equal(t, 30, testObject.Solve())
 
@@ -38,7 +46,9 @@ func Test_ProblemOneReturnsSumOfMultiplesOfNFactors(t *testing.T) {
 
 func Test_ProblemOneReturnsSumOfMultiplesOfNFactorsUnder22(t *testing.T) {
 	// sum(3, 5, 6, 7, 9, 10, 12, 14, 15, 18, 19, 20, 21) = 159
-	testObject := problems.NewProblemOne(22, 3, 5, 7, 19)
+	ceiling := 22
+	factors := []int{3, 5, 7, 19}
+	testObject := problems.NewProblemOne(ceiling, factors[0], factors[1], factors[2], factors[3])
 
 	assert.Equal(t, 159, testObject.Solve())
 
